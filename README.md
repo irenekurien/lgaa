fb63c132ac2208868aeeff3d5eac9b195d4f19b6
irene <irenekurien01@gmail.com>
2022-12-29 18:58:56 +0530

    feat: set up project

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+    <h2 align="center">Legal Guidance Through AI Assistance</h3>
+</p>
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Home = (): JSX.Element => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      )
+    
+};
+
+export default Home;
